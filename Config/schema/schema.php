1c93bb94c51bbaba52a7d818e7297db3e76d9866
ezeller <ezeller@sigis4.intranet.unl>
2014-02-27 09:36:20 -0300

    Mejorada explicación y generado schema

---
+++ Config/schema/schema.php
@@ -0,0 +1,34 @@
+<?php
+/***
+ * Schema for cakephp-viewed
+ * 
+ * Schema para cakephp-viewed
+ * 
+ * @author Esteban Zeller 
+ */
+class ViewedSchema extends CakeSchema {
+
+    public function before($event = array()) {
+        return true;
+    }
+
+    public function after($event = array()) {
+    }
+
+
+    public $viewed = array(
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+        'model' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_spanish_ci', 'charset' => 'utf8'),
+        'model_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+        'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+        'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+        'viewed' => array('type' => 'boolean', 'null' => false, 'default' => null),
+        'modified' => array('type' => 'boolean', 'null' => false, 'default' => null),        
+        'indexes' => array(
+            'PRIMARY' => array('column' => 'id_pregunta', 'unique' => 1 )
+        ),
+        'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_spanish_ci', 'engine' => 'InnoDB')
+    );
+
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 cakephp-viewed
 ==============
 
+[![Build Status](https://travis-ci.org/tranfuga25s/cakephp-viewed.png?branch=master)](https://travis-ci.org/tranfuga25s/cakephp-viewed)
+
 Behaviour para mostrar elementos como vistos o no.
+
+Tiene la capacidad de conocer para cada modelo al cual es agregado que registros estan marcados como vistos y cuales no.
+
+Cada registro tendrá basicamente 3 estados:
+* No visto
+* Visto
+* Modificado
+
+Cada vez que se edite un elemento asociado pasará a modificado.
+Las siguiente sucesiones de estados pueden realizarse:
+* No visto -> visto
+* visto -> modificado
+* Modificado -> visto
+
+Como usar el plugin
+===================
+
+
